a1298e34a535bbc76fb7f8900d8999a98e08465c
Hector Lugo <haze.lugo@gmail.com>
2026-03-27 15:42:35 -0500

    feat: link at-risk student names to profile page

--- src/components/dashboard/at-risk-table.tsx
+++ src/components/dashboard/at-risk-table.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   Table,
   TableBody,
@@ -55,7 +56,9 @@ export function AtRiskTable({ students }: AtRiskTableProps) {
           {students.map((student) => (
             <TableRow key={student.id}>
               <TableCell className="font-medium">
-                {student.lastName}, {student.firstName}
+                <Link href={`/dashboard/students/${student.id}`} className="hover:underline">
+                  {student.lastName}, {student.firstName}
+                </Link>
               </TableCell>
               <TableCell>{GRADE_LABEL[student.gradeLevel] ?? student.gradeLevel}</TableCell>
               <TableCell>
